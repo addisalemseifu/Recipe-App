14ba189303bb6bc7d1d8213d4aea601713cba3e2
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-10-10 18:05:51 +0300

    Add food controller.

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,7 +4,7 @@ class Food < ApplicationRecord
 
   # Validations
   validates :name, presence: true
-  validates :measurment_unit, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :measurment_unit, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
