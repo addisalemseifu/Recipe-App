9fe1fb44030352d9f2c6b498152e91634c0e7fea
tonnytech <tonnytei4@gmail.com>
2023-10-13 14:38:52 +0300

    remove rubocop errors

--- spec/requests/recipe_food_spec.rb
+++ spec/requests/recipe_food_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe '/recipe_foods', type: :request do
     @user = User.create(name: 'Tonny', email: 'tonnytei4@example.com', password: 'Danger123',
                         password_confirmation: 'Danger123')
 
-    @food = Food.create(name: 'Potato', price: 10, measurment_unit: 5, quantity: 6, user:@user)
+    @food = Food.create(name: 'Potato', price: 10, measurment_unit: 5, quantity: 6, user: @user)
 
     @recipe = Recipe.create(name: 'Recipe1', user: @user, preparation_time: 10, description: 'Rece1', cooking_time: 10,
                             public: true)
@@ -37,48 +37,48 @@ RSpec.describe '/recipe_foods', type: :request do
   end
 
   describe 'GET /show' do
-        it 'renders a successful response' do
-          recipe_food = RecipeFood.create! valid_attributes
-          get recipe_recipe_foods_url(recipe_food)
-          expect(response).to be_successful
-        end
-      end
-
-      describe 'GET /new' do
-        it 'renders a successful response' do
-          get new_recipe_recipe_food_url(@recipe)
-          expect(response).to be_successful
-        end
-      end
-
-      describe 'GET /edit' do
-        it 'renders a successful response' do
-          recipe_food = RecipeFood.create! valid_attributes
-          get edit_recipe_recipe_food_url(recipe_id: recipe_food.recipe_id, id: recipe_food.id)
-          expect(response).to be_successful
-        end
-      end
-
-    describe 'POST /create' do
-        context 'with invalid parameters' do
-          it "renders a response with 422 status (i.e. to display the 'new' template)" do
-            post recipe_recipe_foods_url(@recipe), params: { recipe_food: invalid_attributes }
-            expect(response).to have_http_status(:unprocessable_entity)
-          end
-        end
+    it 'renders a successful response' do
+      recipe_food = RecipeFood.create! valid_attributes
+      get recipe_recipe_foods_url(recipe_food)
+      expect(response).to be_successful
     end
+  end
 
-    describe 'PATCH /update' do
-        context 'with valid parameters' do
-            let(:new_attributes) do
-                { recipe: @recipe, food: @food, quantity: 20 }
-            end
-
-            it 'updates the requested recipe_food' do
-                recipe_food = RecipeFood.create! valid_attributes
-                patch recipe_recipe_food_url(@recipe, recipe_food), params: { recipe_food: new_attributes }
-                recipe_food.reload
-            end
-        end
+  describe 'GET /new' do
+    it 'renders a successful response' do
+      get new_recipe_recipe_food_url(@recipe)
+      expect(response).to be_successful
     end
+  end
+
+  describe 'GET /edit' do
+    it 'renders a successful response' do
+      recipe_food = RecipeFood.create! valid_attributes
+      get edit_recipe_recipe_food_url(recipe_id: recipe_food.recipe_id, id: recipe_food.id)
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    context 'with invalid parameters' do
+      it "renders a response with 422 status (i.e. to display the 'new' template)" do
+        post recipe_recipe_foods_url(@recipe), params: { recipe_food: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) do
+        { recipe: @recipe, food: @food, quantity: 20 }
+      end
+
+      it 'updates the requested recipe_food' do
+        recipe_food = RecipeFood.create! valid_attributes
+        patch recipe_recipe_food_url(@recipe, recipe_food), params: { recipe_food: new_attributes }
+        recipe_food.reload
+      end
+    end
+  end
 end
